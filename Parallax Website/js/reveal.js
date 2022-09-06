4a915f18f022f59d534103019ac0556b79c03b6e
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-09-06 18:48:12 -0300

    Update reveal.js

--- Parallax Website/js/reveal.js
+++ Parallax Website/js/reveal.js
@@ -1,1 +1,13 @@
 ScrollReveal().reveal('.logo');
+
+
+var slideUp = {
+    delay: 500,
+    duration: 500,
+    reset: true,
+    opacity: 0
+}
+
+ScrollReveal().reveal('.parallax-1 h1', slideUp)
+ScrollReveal().reveal('.parallax-2 h1', slideUp)
+ScrollReveal().reveal('.parallax-3 h1', slideUp)
