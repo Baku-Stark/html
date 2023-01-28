0860bb077669b8c14986e0b7b0d3f73384f22ff2
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-01-28 17:42:33 -0300

    💻 Update: Dashboard

--- HTML5 e CSS3/Project Idea Beginner/Responsive Admin Dashboard/assets/js/script.js
+++ HTML5 e CSS3/Project Idea Beginner/Responsive Admin Dashboard/assets/js/script.js
@@ -1,7 +1,7 @@
-const chart = document.querySelector('#chart').getContext('2d')
+const ctx = document.querySelector('#show_chart').getContext('2d')
 
 // create a new chart instance
-new chart(chart, {
+new Chart(ctx, {
     type: 'line',
     data: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
@@ -9,13 +9,13 @@ new chart(chart, {
             {
                 label: 'BTC',
                 data: [29374, 33537, 49631, 59095, 57828, 36684, 33572, 39974, 48847, 48116, 61004],
-                borderColor: 'red',
+                borderColor: '#E60501',
                 borderWidth: 2
             },
             {
                 label: 'ETH',
                 data: [31500, 41000, 88000, 26000, 46000, 32698, 5000, 3000, 18656, 24832, 36844],
-                borderColor: 'blue',
+                borderColor: '#1749F0',
                 borderWidth: 2
             }
         ]
